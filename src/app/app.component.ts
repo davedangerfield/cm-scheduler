5b679ba7296810266452c7032961214de1057c71
David Dangerfield <dangerfield@us.ibm.com>
2016-12-27 16:37:00 -0600

    add time-list and change to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { ActivityType, Form, Involvement } from './shared/enums';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';

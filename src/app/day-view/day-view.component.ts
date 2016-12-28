44892eb6b1ca399eacc65e7c06ede03d21a18e5c
David Dangerfield <dangerfield@us.ibm.com>
2016-12-28 04:59:34 -0600

    implement reaction to form selection

--- src/app/day-view/day-view.component.ts
+++ src/app/day-view/day-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 import { Card } from '../shared/model';
+import { Form } from '../shared/enums';
 
 @Component({
   selector: 'app-day-view',
@@ -9,11 +10,26 @@ import { Card } from '../shared/model';
 })
 export class DayViewComponent implements OnInit {
 
-  @Input() cards: Array<Card>;
+  @Input() cards: Card[];
+  
+  private selectedForm: Form;
+  private forms: Form[] = [Form.I, Form.IIA, Form.III];
 
   constructor() { }
 
   ngOnInit() {
+    this.selectedForm = Form.I;
   }
 
+  formName(form: Form) {
+    return Form[form];
+  }
+
+  onChangeForm(selection) {
+    this.selectedForm = selection;
+  }
+
+  formFilter() {
+    return this.cards.filter(card => card.formLevel == this.selectedForm);
+  }
 }

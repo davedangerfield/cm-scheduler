127fb58a463fe5c96e41b6b13199a9bfdefdb48f
David Dangerfield <dangerfield@us.ibm.com>
2016-12-27 19:41:09 -0600

    remove duplication

--- src/app/time-list/time-list.component.ts
+++ src/app/time-list/time-list.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeListComponent implements OnInit {
 
+  times: Array<string> = [
+    '0:00',
+    '0:10',
+    '0:20',
+    '0:30',
+    '0:40',
+    '0:50',
+    '1:00',
+    '1:10',
+    '1:20',
+    '1:30',
+    '1:40',
+    '1:50',
+    '2:00',
+    '2:10',
+    '2:20',
+    '2:30',
+    '2:40',
+    '2:50',
+    '3:00',
+    '3:10',
+    '3:20',
+    '3:30',
+    '3:40',
+    '3:50',
+    '4:00',
+  ];
+
   constructor() { }
 
   ngOnInit() {

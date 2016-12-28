c13858ba9138c25d498702e67f5cd5a159f35ed3
David Dangerfield <dangerfield@us.ibm.com>
2016-12-28 05:36:36 -0600

    add Day enum

---
+++ src/app/shared/enums/day.enum.ts
@@ -0,0 +1,11 @@
+export enum Day {
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday'
+}
+
+export const weekDays = [Day.Monday, Day.Tuesday, Day.Wednesday, Day.Thursday, Day.Friday];
--- src/app/shared/enums/index.ts
+++ src/app/shared/enums/index.ts
@@ -1,3 +1,4 @@
 export * from './activity-type.enum';
+export * from './day.enum';
 export * from './form.enum';
 export * from './involvement.enum';
